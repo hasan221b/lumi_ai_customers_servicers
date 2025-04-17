2e7e0f42fc7abfbdd75bad3109a69c2c5e82de77
Hasan <73844788+hasan221b@users.noreply.github.com>
2025-04-17 03:22:25 -0400

    Update answer_generator.py

--- src/rag/answer_generator.py
+++ src/rag/answer_generator.py
@@ -4,7 +4,7 @@ from src.utils.logger import pipeline_logger
 import re
 
 class AnswerGenerator:
-    def __init__(self, llm, retriever, sentiment_analyzer, zero_shot_model):
+    def __init__(self, llm, retriever, sentiment_analyzer):
         '''
         Setting up answer generator with chat memory and sentiment analysis.
         
@@ -12,12 +12,10 @@ class AnswerGenerator:
             llm: LLM model (e.g., gpt-4o-mini)
             retriever: A retriever method
             sentiment_analyzer: Sentiment analysis pipeline
-            zero_shot_model: Zero-shot classification model
         '''
         self.llm_model = llm
         self.retriever = retriever
         self.sentiment_analyzer = sentiment_analyzer
-        self.zero_shot_model = zero_shot_model
         self.intents = ["shipping", "returns", "payment", "product_info", "other"]
         self.greeting_keywords = ["hello", "hi", "hey", "greetings", "good morning", "good afternoon", "good evening"]
         self.thanking_keywords = ['Thank you','Thanks', 'Ok','bye', 'thx']
@@ -25,15 +23,14 @@ class AnswerGenerator:
             input_variables=['history', 'context', 'question', 'sentiment',
                              'sentimentS', 'suggestions', 'topic', 'needs_clarification'],
             template='''
-            You are an expert E-commerce customer support assistant named Lumi. 
+            you are an expert E-commerce customer support assistant named Lumi. 
             Use the following conversation history and context to answer the question naturally and helpfully:
                 
             Conversation History:\n{history}\n\n
             Context:\n{context}\n\n
             Question:\n{question}\n\n
 
-            if the question is not clear {needs_clarification}
-             use this suggetsions: {suggestions}
+            if the question is not clearuse this suggetsions: {suggestions}
                         '''
         )
         self.rag_chain = (
@@ -44,8 +41,6 @@ class AnswerGenerator:
                 'sentiment': lambda x: self._get_sentiment_label(x['question']),
                 'sentimentS': lambda x: self._get_sentiment_score(x['question']),
                 'suggestions': lambda x: self._retrieve_suggested_context(x['question']),
-                'topic': lambda x: self.classify_intent(x['question']),
-                'needs_clarification': lambda x: self.f_intent_confidence(x['question'])
             }
             | self.prompt
             | self.llm_model
@@ -74,31 +69,11 @@ class AnswerGenerator:
         except Exception as e:
             pipeline_logger.warning(f"Sentiment score retrieval failed: {e}. Defaulting to 0.5.")
             return 0.5
-
-    def classify_intent(self, question):
-        result = self.zero_shot_model(question, self.intents)
-        top_intent = result['labels'][0]
-        top_score = result['scores'][0]
-        # If confidence is low or input is too generic, default to "other"
-        if top_score < 0.7 or len(question.strip()) < 5:  # "Hello" is short and vague
-            return "other"
-        return top_intent
-
-    def f_intent_confidence(self, question):
-        result = self.zero_shot_model(question, self.intents)
-        confidence = result['scores'][0]
-        if confidence < 0.7 or len(question.strip()) < 5:
-            return "I'm not sure what you're asking. How can I assist you today?"
-        return ""
-        
     def is_greeting(self, question):
         """Check if the input is a greeting."""
         question_lower = question.lower().strip()
         return any(keyword in question_lower for keyword in self.greeting_keywords) and len(question_lower.split()) <= 3
-    def is_farewell(self, question):
-        """Check if the input is a greeting."""
-        question_lower = question.lower().strip()
-        return any(keyword in question_lower for keyword in self.thanking_keywords) and len(question_lower.split()) <= 3
+    
     def generator(self, question: str, history: str) -> str:
         '''
         Generate a response using the RAG pipeline with provided history and sentiment analysis.
@@ -117,10 +92,6 @@ class AnswerGenerator:
             if self.is_greeting(question):
                 pipeline_logger.info('Detected greeting, returning simple response')
                 return "Hello! How can I assist you today?"
-            if self.is_farewell(question):
-                pipeline_logger.info('Detected thanking, returning simple response')
-                return "I happy to help you!"
-
             # Proceed with RAG pipeline for non-greetings
             response = self.rag_chain.invoke({'question': question, 'history': history})
             pipeline_logger.info('Response generation completed successfully')
